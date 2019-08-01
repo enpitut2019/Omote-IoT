ad8f5c64692a17079d4ccf11df93601c316909a1
ShinyaHaga <49799123+ShinyaHaga@users.noreply.github.com>
2019-08-01 12:14:19 +0900

    Update Water_supply.sol

--- Water_supply.sol
+++ Water_supply.sol
@@ -1,14 +1,14 @@
 pragma solidity 0.5.10;
 
-contract Waterworks {
+contract Water_supply{
+
+    address public owner;
+    uint public collected_money;//集金の総額
+    mapping (address => MemberStatus) public member;
     
-    
-    
+    struct MemberStatus {
+        uint amount_of_water;//当月の使用量
+        uint diameter;//口径 
+        uint wallet;//個人の財布
+    }
 }
-
-
-
-
-
-
-
