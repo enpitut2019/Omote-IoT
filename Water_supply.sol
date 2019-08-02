25516529b6d2db371be3a45597c2fb206c374cda
hatashogo <s1711392@coins.tsukuba.ac.jp>
2019-08-02 10:31:01 +0900

    支払い機能の変更
    アドレスに支払うように変更しました

--- Water_supply.sol
+++ Water_supply.sol
@@ -2,44 +2,48 @@ pragma solidity 0.5.10;
 
 contract Water_supply {
 
-    address public owner;
-    uint public collected_money;//集金の総額
-    mapping (address => MemberStatus) public member;
-    uint[] public basic_rate = [1296,1620,2700,3510,7560,16200,42120,93960,234900,446040];
+    address payable public owner;
+    uint amount_of_water;//当月の使用量
+    uint diameter;//口径（０から９の数字で） 
+    uint wallet;//財布
+    uint[] public basic_rate = [1296,1620,2700,3510,7560,16200,42120,93960,234900,446040];//基本料金
     
-    struct MemberStatus {
-        uint amount_of_water;//当月の使用量
-        uint diameter;//口径（０から９の数字で） 
-        uint wallet;//個人の財布
+    constructor() public {
+        owner = 0x1cd248fd0CAB42123758e5141ba143894df7f7F3;
     }
+    
+    // modifier onlyOwner(){
+    //     if(msg.sender != owner) revert();
+    //     _;
+    // }
 
     //デポジットされた金額を表示
     function get_wallet() public view returns (uint) {
-        return member[msg.sender].wallet;
+        return wallet;
     }
     
+    //当月の水量を表示
     function get_amount_of_water() public view returns (uint) {
-        return member[msg.sender].amount_of_water;
+        return amount_of_water;
     }
     
     //デポジット
     function deposit() public payable {
         if(msg.value <= 0) revert();
-        member[msg.sender].wallet += msg.value;
+        wallet += msg.value;
     }
     
     //料金の支払い
     function payment() public {
         uint charge = calc_charge();
-        if(member[msg.sender].wallet < charge) revert();
-        member[msg.sender].wallet -= charge;
-        collected_money += charge;
+        if(wallet < charge) revert();
+        owner.transfer(charge);
+        wallet -= charge;
     }
 
     //従量料金の計算（つくば市）
     function calc_commodity_charge(uint _amount_of_water) public view returns (uint){
-        if(member[msg.sender].diameter <= 2){
-
+        if(diameter <= 2){
             if(_amount_of_water <= 10){
                 return 0;
             }else{
@@ -47,7 +51,6 @@ contract Water_supply {
             }
 
         }else{
-
             if(_amount_of_water <= 20){
                 return 151;
             } else if(_amount_of_water <=40) {
@@ -64,11 +67,11 @@ contract Water_supply {
 
     //支払い料金の計算（つくば市）
     function calc_charge() public view returns(uint) {
-        return basic_rate[member[msg.sender].diameter] + calc_commodity_charge(member[msg.sender].amount_of_water) * member[msg.sender].amount_of_water;
+        return basic_rate[diameter] + calc_commodity_charge(amount_of_water) * amount_of_water;
     }
     
     function set(uint _amount_of_water) public {
-        member[msg.sender].amount_of_water = _amount_of_water;
-        member[msg.sender].diameter = 1;
+        amount_of_water = _amount_of_water;
+        diameter = 1;
     }
 }
