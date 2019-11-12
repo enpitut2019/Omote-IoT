e7d88ca1ced1da54175ddb79051b924aa1c760f0
hatashogo <s1711392@coins.tsukuba.ac.jp>
2019-11-13 00:57:22 +0900

    デモ用の表示を追加

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -11,8 +11,9 @@
 #define MY_ADDRESS "0x61666605cE04f4D5e845165692D8a71C026d9a34"
 #define CONTRACT_ADDRESS "0x200Ed6bc284F778E6c4ad3A22dE0ddc5b2a8239a"
 #define ETHERSCAN_TX "https://rinkeby.etherscan.io/tx/"
-
+//________________
 //const char* ssid = "eduroam"; // Eduroam SSID
+//________________
 const char ssid[] = "logitec2nd49";//_____追加
 const char passwd[] = "1C3HCA519832B";//_____追加
 const char* host = "arduino.php5.sk"; //external server domain for HTTP connection after authentification
@@ -43,12 +44,12 @@ void setup() {
 
 
 void loop(){
+  
  if(timer()){
-     Serial.print("waterFlow:");
-     Serial.print(waterFlow);
-     Serial.println("   L");
-     //set_used_water(waterFlow);
-     delay(500);
+    Serial.print("waterFlow: ");
+    Serial.print(waterFlow);
+    Serial.println(" ml");
+    set_used_water(waterFlow);
  }
 }
 
@@ -56,6 +57,7 @@ void ConnectWiFi(){
     Serial.println("");
     Serial.print("Connecting to network: ");
     Serial.println(ssid);
+//_____________________
 //    WiFi.disconnect(true);  //disconnect form wifi to set new wifi connection
 //    WiFi.mode(WIFI_STA); //init wifi mode
 //    esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY)); //provide identity
@@ -64,6 +66,7 @@ void ConnectWiFi(){
 //    esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT(); //set config settings to default
 //    esp_wifi_sta_wpa2_ent_enable(&config); //set config settings to enable function
 //    WiFi.begin(ssid); //connect to wifi
+//______________________
     WiFi.begin(ssid, passwd); //____________追加
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
@@ -78,73 +81,107 @@ void ConnectWiFi(){
     Serial.println("WiFi connected");
     Serial.println("IP address set: "); 
     Serial.println(WiFi.localIP()); //print LAN IP
+    Serial.println("");
 }
 
 void set_used_water(int _waterFlow){
+//  Serial.println("Sending Transaction...");
+//  Contract contract(&web3, "");
+//  contract.SetPrivateKey(PRIVATE_KEY);
+//  string addr = MY_ADDRESS;
+//  uint32_t nonceVal = (uint32_t)web3.EthGetTransactionCount(&addr); //obtain the next nonce
+//  uint256_t weiValue = 10; //send 0.25 eth
+//  unsigned long long gasPriceVal = 1000000000ULL;
+//  uint32_t  gasLimitVal = 90000;
+//  string emptyString = "";
+//  string toAddress = "0xcE192a3cA53853f49c003d7ddcc63fDfA4c8adC6";
+//  string result = contract.SendTransaction(nonceVal, gasPriceVal, gasLimitVal, &toAddress, &weiValue, &emptyString);
+//  Serial.println(result.c_str());
+//
+//  string transactionHash = web3.getString(&result);
+//  Serial.println("TX on Etherscan:");
+//  Serial.print(ETHERSCAN_TX);
+//  Serial.println(transactionHash.c_str());
+//  Serial.println("---トランザクションの送信に成功---");
+//  Serial.println("");
+
+//_____________________
+  Serial.println("Sending Transaction...");
   Contract contract(&web3,CONTRACT_ADDRESS);
   contract.SetPrivateKey(PRIVATE_KEY);
   string addr = MY_ADDRESS;
-
   uint32_t nonceVal = (uint32_t)web3.EthGetTransactionCount(&addr);
-  unsigned long long gasPriceVal = 141006540;
+  unsigned long long gasPriceVal = 141006540ULL;
   uint32_t gasLimitVal = 3000000;
   uint8_t dataStr[100];
   memset(dataStr, 0, 100);
   string toStr = CONTRACT_ADDRESS;
-  uint256_t valueStr = "0x00";
+  string valueStr = "0x00";
   string p = contract.SetupContractData("set_used_water(uint256)", _waterFlow);
-
   string result = contract.SendTransaction(nonceVal, gasPriceVal, gasLimitVal, &toStr, &valueStr, &p);
 
   Serial.println(result.c_str());
-
   string transactionHash = web3.getString(&result);
   Serial.println("TX on Etherscan:");
   Serial.print(ETHERSCAN_TX);
   Serial.println(transactionHash.c_str());
+  Serial.println("---トランザクションの送信に成功---");
+  Serial.println("");
+//__________________________
 }
 
 
 //10秒前が同じ月かどうか判断する=>月が変わった時に
 bool timer(){
-  // 曜日文字列配列
-  static const char *pszWDay[] = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+//  // 曜日文字列配列
+//  static const char *pszWDay[] = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+//
+//  // 現在時刻の取得
+//  time_t timeNow = time(NULL);
+//  struct tm* tmNow = localtime(&timeNow);
+//
+//  
+//  char past_Date[32];
+//  char present_Date[32];
+//  sprintf(present_Date, "%04d/%02d",
+//          tmNow->tm_year+1900,
+//          tmNow->tm_mon+1);
+//
+//
+//  // 時刻
+//  char szTime1[32];
+//  char szTime2[32];
+//  sprintf( szTime1, "%02d:%02d",
+//          tmNow->tm_hour,
+//          tmNow->tm_min
+//          );
+//
+//
+////  Serial.print(szTime1);
+//
+//  char *s1 = szTime1;
+//  char *s2 = szTime2;
+//
+//  if(strcmp(s1,s2) == 0){
+//      return true;
+//    }else{
+//      return false;
+//    }
+//    
+//  szTime2[32] = szTime1[32];
+//  //past_Date[32] = present_Date[32];
+//  delay(10000);
 
-  // 現在時刻の取得
-  time_t timeNow = time(NULL);
-  struct tm* tmNow = localtime(&timeNow);
-
-  
-  char past_Date[32];
-  char present_Date[32];
-  sprintf(present_Date, "%04d/%02d",
-          tmNow->tm_year+1900,
-          tmNow->tm_mon+1);
-
-
-  // 時刻
-  char szTime1[32];
-  char szTime2[32];
-  sprintf( szTime1, "%02d:%02d",
-          tmNow->tm_hour,
-          tmNow->tm_min
-          );
-
-
-  Serial.print(szTime1);
-
-  char *s1 = szTime1;
-  char *s2 = szTime2;
-
-  if(strcmp(s1,s2) == 0){
-      return true;
-    }else{
-      return false;
-    }
-    
-  szTime2[32] = szTime1[32];
-  //past_Date[32] = present_Date[32];
-  delay(10000);
+//デモ用_______________
+  int second = 15;//水量を計る時間の設定
+  Serial.println("計量中");
+  for (int i=0; i<second; i++){
+    Serial.print(".");
+    delay(1000);
+   }
+  Serial.println("");
+  return true;
+//____________________
 }
 
 void pulse()   //measure the quantity of square wave
