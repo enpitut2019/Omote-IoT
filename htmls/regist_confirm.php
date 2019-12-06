314670199be7e46c5a86b1ff39de3b9cefeb4ed6
keigokimura <ksncth26i14@gmail.com>
2019-12-06 16:19:39 +0900

    ヘッダー、フッターの追加
    登録確認ページ、解約ページにヘッダー、フッター追加

--- htmls/regist_confirm.php
+++ htmls/regist_confirm.php
@@ -4,16 +4,49 @@
     <title>水道管理アプリ</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
-    <link href="./server.css" rel="stylesheet">
-    <link href="css/registpage.css" rel="stylesheet">
-    <script src="js/bootstrap.bundle.js"></script>
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <script src="./serverindex.js"></script>
-    <script src="js/main1.js"></script>
+    <link href="../css/server.css" rel="stylesheet">
+    <link href="../styles/registpage.css" rel="stylesheet">
+    <script src="../js/bootstrap.bundle.js"></script>
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <script src="../scripts/serverindex.js"></script>
+    <link href="../styles/common.css" rel="stylesheet">
+    <script src="../scripts/common.js"></script>
 </head>
 <body>
-<header class="head">
-    <h1><b>水道自動決済アプリケーション管理ページ</h1>
+<header>
+    <nav class="navbar navbar-expand-md">
+        <div class="px-5">
+            <a class="navbar-brand" href="/"><img
+                        src="../images/omotekensuido.png" alt="面研水道"></a>
+        </div>
+        <button type="button" class="navbar-toggler" data-toggle="collapse"
+                data-target="#bs-navi" aria-controls="bs-navi" aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="bs-navi">
+            <ul class="navbar-nav ml-auto"></ul>
+            <ul class="navbar-nav mr-5">
+                <div class="px-4 h5">
+                    <li class="nav-item" style="border-bottom: 1.5px solid #f5f5f5"><a
+                                class="nav-link" href="../..">ホーム</a></li>
+                </div>
+                <div class="px-4 h5">
+                    <li class="nav-item" style="border-bottom: 1.5px solid #f5f5f5"><a
+                                class="nav-link" href="#">使い方</a></li>
+                </div>
+                <div class="px-4 h5">
+                    <li class="nav-item" style="border-bottom: 1.5px solid #f5f5f5"><a
+                                class="nav-link" href="#">登録情報</a></li>
+                </div>
+                <div class="px-4 h5">
+                    <li class="nav-item" style="border-bottom: 1.5px solid #f5f5f5"><a
+                                class="nav-link" href="../htmls/charge.html">入金</a></li>
+                </div>
+            </ul>
+        </div>
+    </nav>
 </header>
 <div class="main">
     <div class="mx-auto" style="width: 600px;">
@@ -83,7 +116,20 @@
     </div>
 </div>
 <a href="waterserver_index.php">戻る</a>
-<footer class="footer">omete_IoT</footer>
+<footer>
+    <div class="bottom section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-3 mx-auto">
+                    <a class="navbar-brand"
+                       href="http://www.risk.tsukuba.ac.jp/omote-lab/"><img
+                                src="../images/omotelab-tsukuba-2.png" class="img-fluid"
+                                alt="omotelab"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
 
