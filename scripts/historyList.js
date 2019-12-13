994b3afff7758ce39e0f72a66ab0a89f71e59158
hatashogo <s1711392@coins.tsukuba.ac.jp>
2019-12-13 16:42:13 +0900

    全期間の単位変換

--- scripts/historyList.js
+++ scripts/historyList.js
@@ -1,13 +1,19 @@
+var waterHistory;
+var chargeHistory;
+var chargeHistoryJpy;
+var state;
+
 //全期間リスト表示
 function dispList(amount) {
 	var now   = new Date();
 	var year  = now.getFullYear();
 	var month = now.getMonth();
-	var waterHistoy = amount[0];
-	var chargeHistory = amount[1];
+	waterHistory = amount[0];
+	chargeHistory = amount[1];
 	var len = waterHistory.length;
-	label = [];
-	inc = 0;
+	chargeHistoryJpy = [];
+	var label = [];
+	var inc = 0;
 	for (var i = 0; i < len; i++) {
 		label.push(year - Math.floor(len / 12) + inc + "年" + ((month + 12 - (len - i) % 12) % 12 + 1) + "月");
 		if ((len - i) % 12 == 0) {
@@ -15,24 +21,6 @@ function dispList(amount) {
 		}
 	}
 	var tbody = document.getElementById('tbodyID');
-	for (var i = 0; i < len; i++) {
-        var tr = document.createElement('tr');
-        var td = document.createElement('td');
-        td.innerHTML = label[i];
-        tbody.appendChild(td);
-        var td = document.createElement('td');
-        td.innerHTML = waterHistory[i];
-        tbody.appendChild(td);
-        var td = document.createElement('td');
-        td.innerHTML = chargeHistory[i];
-        tbody.appendChild(td);
-        tbody.appendChild(tr);
-	}
-}
-
-//ethereumと円の変換
-function convertEthToJpy() {
-	chargeHistoryEth = [];
 	$.ajax({
 		  type: 'GET',
 		  url: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=jpy&ids=ethereum',
@@ -41,16 +29,45 @@ function convertEthToJpy() {
 			  Ethereum = json[0];
 			  current_price = Ethereum["current_price"];
 			  console.log(current_price)
-
+			  console.log(len)
+			  for (var i = 0; i < len; i++) {
+				 // chargeHistoryJpy.push(chargeHistory[i] * current_price * Math.pow(10, -18));
+				  chargeHistoryJpy.push(Math.floor(chargeHistory[i] * current_price * Math.pow(10, -4))/10);
+			  }
+				for (var i = 0; i < len; i++) {
+			        var tr = document.createElement('tr');
+			        tr.innerHTML = '<tr><td>'+label[i]+'</td><td>'+waterHistory[i]+'</td><td>'+chargeHistoryJpy[i]+'</td></tr>';
+			        tbody.appendChild(tr);
+				}
+				state = true;
 		  }
-		});
+	});
 }
+
+//ethereumと円の変換
+function convertEthToJpy() {
+	len = waterHistory.length;
+	var tbody = document.getElementById('tbodyID');
+	if(state) {
+		$('#unit').text("料金(wei)");
+		for (var i = 0; i < len; i++) {
+			tbody.rows[i].cells[2].innerText = chargeHistory[i];
+		}
+		state = false;
+	} else {
+		$('#unit').text("料金(JPY)");
+		for (var i = 0; i < len; i++) {
+			tbody.rows[i].cells[2].innerText = chargeHistoryJpy[i];
+		}
+		state = true;
+	}
+}
+
 
 $(function(){
 	init().then(result => {
 		getHistoryofWater().then(result => {
 			dispList(result);
-			convertEthToJpy();
 		});
 	});
 });
