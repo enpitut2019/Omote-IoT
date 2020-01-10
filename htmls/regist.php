a19488e73bba688758ce64779a995c00373c6a94
keigokimura <ksncth26i14@gmail.com>
2020-01-10 11:44:57 +0900

    php関数呼び出し

--- htmls/regist.php
+++ htmls/regist.php
@@ -25,7 +25,6 @@ function insert(){
     $stmh = $pdo->prepare($sql);
     $params = array(':name' => $name, ':tel' => $tel, ':address' => $address, ':mail' => $mail, ':eth' => $acounteth, ':password' => $password);
     $stmh->execute($params);
-    echo '登録されました。';
 }
 
 if(isset($_POST['submit'])){
@@ -39,8 +38,6 @@ if(isset($_POST['submit'])){
     <title>水道管理アプリ</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
-    <link href="../styles/server.css" rel="stylesheet">
-    <link href="../styles/registpage.css" rel="stylesheet">
     <script src="../scripts/bootstrap.bundle.js"></script>
     <link href="../styles/bootstrap.css" rel="stylesheet">
     <script src="../scripts/serverindex.js"></script>
@@ -48,6 +45,42 @@ if(isset($_POST['submit'])){
     <script src="../scripts/common.js"></script>
 </head>
 <body>
+<header>
+    <nav class="navbar navbar-expand-md">
+        <div class="px-md-5">
+            <a class="navbar-brand" href="/"><img
+                        src="../images/omotekensuido.png" alt="面研水道"></a>
+        </div>
+        <button type="button" class="navbar-toggler" data-toggle="collapse"
+                data-target="#bs-navi" aria-controls="bs-navi" aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="bs-navi">
+            <ul class="navbar-nav ml-lg-auto"></ul>
+            <ul class="navbar-nav mr-lg-5">
+                <div class="px-md-2 px-lg-4 h5">
+                    <li class="nav-item borderBottom"><a
+                                class="nav-link" href="../">ホーム</a></li>
+                </div>
+                <div class="px-md-2 px-lg-4 h5">
+                    <li class="nav-item borderBottom"><a
+                                class="nav-link" href="#">使い方</a></li>
+                </div>
+                <div class="px-md-2 px-lg-4 h5">
+                    <li class="nav-item borderBottom"><a
+                                class="nav-link" href="./information_confirm.php">登録情報</a></li>
+                </div>
+                <div class="px-md-2 px-lg-4 h5">
+                    <li class="nav-item borderBottom"><a
+                                class="nav-link" href="./charge.html">入金</a></li>
+                </div>
+            </ul>
+        </div>
+    </nav>
+</header>
+<h2 class="registed_h2">登録されました</h2>
 <a href="../index.html">使用量確認ページはこちら</a>
 <footer>
     <div class="bottom section-padding">
