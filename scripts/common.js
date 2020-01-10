a307733e10ed1aa35bdb00fe9b2b2721c6760578
hatashogo <s1711392@coins.tsukuba.ac.jp>
2020-01-10 15:02:02 +0900

    gwei

--- scripts/common.js
+++ scripts/common.js
@@ -36,7 +36,7 @@ function dispUserwallet() {
 				$('#balance').text((Math.floor(currentPrice * result * Math.pow(10, -17))/10).toLocaleString() + '(JPY)')
 //				$('#balance').text(Math.floor((currentPrice * result * Math.pow(10, -4))/10).toLocaleString() + '(JPY)')//デモ用
 			} else {
-				$('#balance').text(Math.floor(result).toLocaleString() + '(wei)')
+				$('#balance').text((Math.floor(result * Math.pow(10, -8))/10).toLocaleString() + '(gwei)')
 			}
 		}
 	});
