f306a0a0068c999c25b4f054ffdc61ae7b75b25d
hatashogo <s1711392@coins.tsukuba.ac.jp>
2020-01-10 13:00:27 +0900

    登録ページの整理

--- htmls/information.php
+++ htmls/information.php
@@ -4,8 +4,6 @@
     <title>水道管理アプリ</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
-    <link href="../styles/server.css" rel="stylesheet">
-    <link href="../styles/registpage.css" rel="stylesheet">
     <script src="../scripts/bootstrap.bundle.js"></script>
     <link href="../styles/bootstrap.css" rel="stylesheet">
     <script src="../scripts/serverindex.js"></script>
@@ -13,45 +11,47 @@
     <script src="../scripts/common.js"></script>
 </head>
 <body>
-<header>
-    <nav class="navbar navbar-expand-md">
-        <div class="px-5">
-            <a class="navbar-brand" href="/"><img
-                    src="../images/omotekensuido.png" alt="面研水道"></a>
-        </div>
-        <button type="button" class="navbar-toggler" data-toggle="collapse"
-                data-target="#bs-navi" aria-controls="bs-navi" aria-expanded="false"
-                aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+	<header>
+		<nav class="navbar navbar-expand-md">
+			<div class="px-md-5">
+				<a class="navbar-brand" href="/"><img
+					src="../images/omotekensuido.png" alt="面研水道"></a>
+			</div>
+			<button type="button" class="navbar-toggler" data-toggle="collapse"
+				data-target="#bs-navi" aria-controls="bs-navi" aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
-        <div class="collapse navbar-collapse" id="bs-navi">
-            <ul class="navbar-nav ml-auto"></ul>
-            <ul class="navbar-nav mr-5">
-                <div class="px-4 h5">
-                    <li class="nav-item" style="border-bottom: 1.5px solid #f5f5f5"><a
-                            class="nav-link" href="../">ホーム</a></li>
-                </div>
-                <div class="px-4 h5">
-                    <li class="nav-item" style="border-bottom: 1.5px solid #f5f5f5"><a
-                            class="nav-link" href="./howto.html">使い方</a></li>
-                </div>
-                <div class="px-4 h5">
-                    <li class="nav-item" style="border-bottom: 1.5px solid #f5f5f5"><a
-                            class="nav-link" href="../htmls/information_confirm.php">登録情報</a></li>
-                </div>
-                <div class="px-4 h5">
-                    <li class="nav-item" style="border-bottom: 1.5px solid #f5f5f5"><a
-                            class="nav-link" href="../htmls/charge.html">入金</a></li>
-                </div>
-            </ul>
-        </div>
-    </nav>
-</header>
-<div class="main">
-    <div class="mx-auto" style="width: 600px;">
-
-        <h2>情報確認ページ</h2>
+			<div class="collapse navbar-collapse" id="bs-navi">
+				<ul class="navbar-nav ml-lg-auto"></ul>
+				<ul class="navbar-nav mr-lg-5">
+					<div class="px-md-2 px-lg-4 h5">
+						<li class="nav-item borderBottom"><a
+							class="nav-link" href="../">ホーム</a></li>
+					</div>
+					<div class="px-md-2 px-lg-4 h5">
+						<li class="nav-item borderBottom"><a
+							class="nav-link" href="./howto.html">使い方</a></li>
+					</div>
+					<div class="px-md-2 px-lg-4 h5">
+						<li class="nav-item borderBottom"><a
+							class="nav-link" href="./information_confirm.php">登録情報</a></li>
+					</div>
+					<div class="px-md-2 px-lg-4 h5">
+						<li class="nav-item borderBottom"><a
+							class="nav-link" href="./charge.html">入金</a></li>
+					</div>
+				</ul>
+			</div>
+		</nav>
+	</header>
+	<br><br><br>
+	<h3 class="text-center">登録情報</h3>
+	<br>
+	<div class="mx-auto bdr-info">
+		<div class="border border-dark rounded p-4">
+        <br>
 
         <?php
 
@@ -72,45 +72,50 @@
 
 
         ?>
-        <table>
-            <tbody>
-                <div class="form-group">
-                    <label>名前:</label><br>
-                    <input type="text" readonly class="form-control" value='<?= htmlspecialchars($stmh->fetch(PDO::FETCH_ASSOC)['name']) ?>'>
-                </div>
-                <div class="form-group">
-                    <label>電話番号</label><br>
-                    <input type="text" readonly class="form-control" value='<?= htmlspecialchars($stmh->fetch(PDO::FETCH_ASSOC)['tel']) ?>'>
-                </div>
-                <div class="form-group">
-                    <label>住所</label><br>
-                    <input type="text" readonly class="form-control" value='<?= htmlspecialchars($stmh->fetch(PDO::FETCH_ASSOC)['address']) ?>'>
-                </div>
-                <div class="form-group">
-                    <label>メールアドレス</label><br>
-                    <input type="text" readonly class="form-control" value='<?= htmlspecialchars($stmh->fetch(PDO::FETCH_ASSOC)['mail']) ?>'>
-                </div>
-                <div class="form-group">
-                    <label>ethのアカウント</label><br>
-                    <input type="text" readonly class="form-control" value='<?= htmlspecialchars($stmh->fetch(PDO::FETCH_ASSOC)['eth']) ?>'>
-                </div>
-            </tbody>
-        </table>
+	        <table>
+	            <tbody>
+	                <div class="form-group">
+	                    <label>名前:</label><br>
+	                    <input type="text" readonly class="form-control" value='<?= htmlspecialchars($stmh->fetch(PDO::FETCH_ASSOC)['name']) ?>'>
+	                </div>
+	                <div class="form-group">
+	                    <label>電話番号</label><br>
+	                    <input type="text" readonly class="form-control" value='<?= htmlspecialchars($stmh->fetch(PDO::FETCH_ASSOC)['tel']) ?>'>
+	                </div>
+	                <div class="form-group">
+	                    <label>住所</label><br>
+	                    <input type="text" readonly class="form-control" value='<?= htmlspecialchars($stmh->fetch(PDO::FETCH_ASSOC)['address']) ?>'>
+	                </div>
+	                <div class="form-group">
+	                    <label>メールアドレス</label><br>
+	                    <input type="text" readonly class="form-control" value='<?= htmlspecialchars($stmh->fetch(PDO::FETCH_ASSOC)['mail']) ?>'>
+	                </div>
+	                <div class="form-group">
+	                    <label>イーサリアムアドレス</label><br>
+	                    <input type="text" readonly class="form-control" value='<?= htmlspecialchars($stmh->fetch(PDO::FETCH_ASSOC)['eth']) ?>'>
+	                </div>
+	                <div class="form-group">
+	                    <label>パスワード</label><br>
+	                    <input type="password" readonly class="form-control" value='<?= htmlspecialchars($stmh->fetch(PDO::FETCH_ASSOC)['password']) ?>'>
+	                </div>
+	            </tbody>
+	        </table>
+	    </div>
     </div>
-</div>
-
-<footer>
-    <div class="bottom section-padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-3 mx-auto">
-                    <a class="navbar-brand"
-                       href="http://www.risk.tsukuba.ac.jp/omote-lab/"><img
-                            src="../images/omotelab-tsukuba-2.png" class="img-fluid"
-                            alt="omotelab"></a>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
+	<br>
+	<footer>
+		<div class="bottom section-padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-6 col-md-3 mx-auto">
+						<a class="navbar-brand"
+							href="http://www.risk.tsukuba.ac.jp/omote-lab/"><img
+							src="../images/omotelab-tsukuba-2.png" class="img-fluid"
+							alt="omotelab"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
 </body>
+</html>
